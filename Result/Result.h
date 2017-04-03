4044b6f57591b866b5804692ee3bbac0de5132d0
George Webster <ethyreal@Georges-MacBook-Pro.local>
2017-04-02 20:53:06 -0700

    added Basic Result Type
    created framework and unit test targets

---
+++ Result/Result.h
@@ -0,0 +1,19 @@
+//
+//  Result.h
+//  Result
+//
+//  Created by George Webster on 4/2/17.
+//  Copyright © 2017 George Webster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Result.
+FOUNDATION_EXPORT double ResultVersionNumber;
+
+//! Project version string for Result.
+FOUNDATION_EXPORT const unsigned char ResultVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Result/PublicHeader.h>
+
+
---
+++ Result/Result.swift
@@ -0,0 +1,88 @@
+//
+//  Result.swift
+//  Result
+//
+//  Created by George Webster on 4/2/17.
+//  Copyright © 2017 George Webster. All rights reserved.
+//
+
+import Foundation
+
+enum Result<T> {
+    
+    case success(T)
+    case failure(Error)
+    
+}
+
+// MARK:- Accessors
+
+extension Result {
+    
+    var isSuccess:Bool {
+        switch self {
+        case .success: return true
+        case .failure: return false
+        }
+    }
+    
+    var isFailure:Bool {
+        switch self {
+        case .success: return false
+        case .failure: return true
+        }
+    }
+    
+    var error:Error? {
+        switch self {
+        case let .failure(error):
+            return error
+        case .success:
+            return nil
+        }
+    }
+    
+    var value:T? {
+        switch self {
+        case .success( let value):
+            return value
+        case .failure:
+            return nil
+        }
+    }
+
+}
+
+// MARK:- Monads
+
+extension Result {
+
+    func map<U>(_ transform: (T) throws -> U) -> Result<U> {
+        
+        switch self {
+        case let .failure(error):
+            return .failure(error)
+        case let .success(value):
+            do {
+                return .success( try transform(value) )
+            }
+            catch let error {
+                return .failure(error)
+            }
+        }
+    }
+
+    func flatMap<U>(_ transform:(T) -> Result<U>) -> Result<U> {
+        
+        switch self {
+        case let .failure(error):
+            return .failure(error)
+        case let .success(value):
+            return transform(value)
+        }
+    }
+    
+}
+
+
+
